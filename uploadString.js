0209b1ccc7babfa00793eb55893ffd9b9f1b4034
Patrick Desmarais <patrick.desm@gmail.com>
2017-06-17 16:52:29 -0400

    Upload a simple string as a file on S3

---
+++ uploadString.js
@@ -0,0 +1,23 @@
+var config = require('./config.json');
+
+var AWS = require('aws-sdk');
+
+var s3 = new AWS.S3();
+
+var bucket = config['bucket'];
+var key = 'hello_world.txt';
+
+var params = {
+  Bucket: bucket,
+  Key: key,
+  Body: 'Hello World!'
+};
+
+s3.putObject(params, function (err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("Successfully uploaded data to " + bucket + "/" + key);
+});
